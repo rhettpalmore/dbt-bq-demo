de367e10ed3909e5d1184d93869f78159473a568
Rhett Palmore <palmore.rhett@gmail.com>
2026-02-16 17:42:55 +0000

    file added

---
+++ models/example/stg_customers.sql
@@ -0,0 +1,11 @@
+With stg_customers AS (
+    SELECT 
+        customer_id, 
+        CONCAT(first_name, ' ', last_name) AS customer_name, 
+        email AS email_address, 
+        address AS billing_address
+    FROM raw.customer_id
+)
+
+SELECT * 
+FROM stg_customers
